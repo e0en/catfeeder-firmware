e497fe3f7d754c76093a7b99460e958cff478727
Yoonseop Kang <e0engoon@gmail.com>
2024-02-05 18:56:05 +0900

    Implement simple stepper forward movement

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,28 @@
 #include "pins_arduino.h"
 #include <Stepper.h>
 
-const int ledPin = 13;
-const int stepsPerRevolution = 200;
-Stepper motor(stepsPerRevolution, 8, 9, 10, 11);
+const int ledPin = LED_BUILTIN;
 
-void setup() { pinMode(ledPin, OUTPUT); }
+const int step_per_rev = 200;
+const int motor_a_1_pin = 2;
+const int motor_a_2_pin = 3;
+const int motor_b_1_pin = 4;
+const int motor_b_2_pin = 5;
+
+Stepper stepper(step_per_rev, motor_a_1_pin, motor_a_2_pin, motor_b_1_pin,
+                motor_b_2_pin);
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  stepper.setSpeed(60);
+}
 
 void loop() {
-  motor.step(stepsPerRevolution);
   digitalWrite(ledPin, HIGH);
+  stepper.step(-step_per_rev);
   delay(500);
+
   digitalWrite(ledPin, LOW);
+  stepper.step(-step_per_rev);
   delay(500);
 }
